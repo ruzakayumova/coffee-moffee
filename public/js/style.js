5600eed4caf292f12416ababd426461f20e1b757
ruza <feruza.abdukayumova00@gmail.com>
2021-08-15 21:35:41 +0500

    init

---
+++ public/js/style.js
@@ -0,0 +1,19 @@
+$(function() {
+    var $win = $(window),
+        $fv = $('.hero'),
+        $header = $('.header')
+        fvHeight = $fv.outerHeight(),
+        fixedClass = 'fixed';
+
+  $win.on('load scroll',function(){
+    var value = $(this).scrollTop();
+        if($win.width()>768)
+
+            if (value > fvHeight) {
+                $header.addClass(fixedClass);
+
+            } else {
+                $header.removeClass(fixedClass);
+            }
+    })
+  });
